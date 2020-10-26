bfbce7875cd54aa781faaa818cb58a6c7311a102
hamidsultanzadeh <sultanzade.06@mail.ru>
2020-10-26 22:41:45 +0400

    test-controller

---
+++ src/test/java/com/webperside/brogrammersspecialforum/BrogrammersSpecialForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webperside.brogrammersspecialforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrogrammersSpecialForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
