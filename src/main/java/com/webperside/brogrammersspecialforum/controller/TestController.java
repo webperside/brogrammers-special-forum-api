cccee5428f48594f30af059840aa6fc20cab3de8
hamidsultanzadeh <sultanzade.06@mail.ru>
2020-10-30 13:41:15 +0400

    auth-endpoint for react test

--- src/main/java/com/webperside/brogrammersspecialforum/controller/TestController.java
+++ src/main/java/com/webperside/brogrammersspecialforum/controller/TestController.java
@@ -2,6 +2,7 @@ package com.webperside.brogrammersspecialforum.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,10 @@ public class TestController {
     @GetMapping
     public String index(){
         return "<h1><i>Hello from the other side</i></h1>";
+    }
+    
+    @GetMapping("/auth")
+    public boolean auth(@RequestParam(value = "isAuthenticated",defaultValue = "false") boolean isAuthenticated){
+        return isAuthenticated;
     }
 }
