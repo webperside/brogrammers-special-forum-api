8264ef7c675090c33e3632fc9afadee4d37057d3
hamidsultanzadeh <sultanzade.06@mail.ru>
2020-11-10 16:35:15 +0400

    set category id

--- src/main/java/com/webperside/brogrammersspecialforum/dto/response/CategoryDto.java
+++ src/main/java/com/webperside/brogrammersspecialforum/dto/response/CategoryDto.java
@@ -8,12 +8,14 @@ import lombok.Data;
 @Builder
 public class CategoryDto {
 
+    private Integer id;
     private String name;
     private String urlName;
     private Integer titlesNumber;
 
     public static CategoryDto fromEntity(Category category){
         return CategoryDto.builder()
+                .id(category.getId())
                 .name(category.getName())
                 .urlName(category.getUrlName())
                 .titlesNumber(category.getTitles().size())
